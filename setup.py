98300adbcf7cc9450927c1ac31d02e69475b47d1
Duncan McGreggor <oubiwann@gmail.com>
2008-02-27 01:46:49 +0000

    * Renamed package to pyrtf-ng. * Updated setup.py.

--- setup.py
+++ setup.py
@@ -1,45 +1,42 @@
-"""PyRTF - Rich Text Format Document Generation
+"""pyrtf-ng - The next generation in Rich Text Format documents for Python.
 
-PyRTF is a pure python module for the efficient generation of rich text format
-documents. Supports styles, tables, cell merging, jpg and png images and tries
-to maintain compatibility with as many RTF readers as possible. """
+pyrtf-ng is a pure python module for the efficient creation and parsing of rich
+text format documents. Supports styles, tables, cell merging, jpg and png
+images and tries to maintain compatibility with as many RTF readers as
+possible. """
 
-classifiers = """\
-Development Status :: 4 - Beta
-Topic :: Text Editors :: Text Processing
-Topic :: Software Development :: Libraries :: Python Modules
-Intended Audience :: Developers
-Programming Language :: Python
-License :: OSI Approved :: GNU General Public License (GPL)
-License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)
-"""
-
-import	sys
+import    sys
 from distutils.core import setup
 
-if sys.version_info < (2, 3):
-    _setup = setup
-    def setup(**kwargs):
-        if kwargs.has_key("classifiers"):
-            del kwargs["classifiers"]
-        _setup(**kwargs)
 
 doclines = __doc__.split("\n")
 
-setup( name		= 'PyRTF',
-       version	        = '0.45',
-       author           = 'Simon Cusack',
-       author_email     = 'scusack@sourceforge.net',
-       url              = 'http://pyrtf.sourceforge.net/',
-       license	        = 'http://www.gnu.org/licenses/gpl.html',
-       platforms	= [ 'Any' ],
-       description	= doclines[0],
-       classifiers	= filter( None, classifiers.split( '\n' ) ),
+setup(name = 'PyRTF',
+       version = '1.0.0',
+       author = 'Duncan McGreggor',
+       author_email = 'oubiwann@adytum.us',
+       url = 'http://code.google.com/p/pyrtf-ng/',
+       license = 'MIT'
+       platforms    = [ 'Any' ],
+       description    = doclines[0],
        long_description = '\n'.join( doclines[2:] ),
        keywords         = ( 'RTF',
                             'Rich Text',
                             'Rich Text Format',
-                            'documentation',
-                            'reports' ),
-       packages         = [ 'PyRTF', ],
-       package_dir      = { '' : '.' } )
+                            'documents',
+                            'word' ),
+    packages = [
+        'pyrtfng',
+        ],
+    package_dir = { i
+        'pyrtfng' : 'pyrtfng'
+    },
+    classifiers = [f.strip() for f in """
+        Development Status :: 4 - Beta
+        Topic :: Text Editors :: Text Processing
+        Topic :: Software Development :: Libraries :: Python Modules
+        Intended Audience :: Developers
+        Programming Language :: Python
+        License :: OSI Approved :: MIT
+    """]
+    )
