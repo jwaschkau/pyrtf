1c568ed4767e5f52ec3aabf357a676fbc64c05fe
Duncan McGreggor <oubiwann@gmail.com>
2008-03-01 05:46:38 +0000

    * Added the charater tests.

--- test/document/test_charaters.py
+++ test/document/test_charaters.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
 from rtfng.utils import RTFTestCase
 from rtfng.Elements import Document
+from rtfng.PropertySets import TextPropertySet
+
+from rtfng.document.character import TEXT, Text
+from rtfng.document.section import Section
+from rtfng.document.paragraph import Paragraph
 
 def initializeDoc():
     doc = Document()
@@ -9,49 +14,50 @@ def initializeDoc():
     return (doc, section, doc.StyleSheet)
 
 class CharacterTestCase(RTFTestCase):
-    def make_char():
+
+    def make_charStyleOverride():
         doc, section, styles = initializeDoc()
         p = Paragraph()
-        p.append('It is also possible to provide overrides for element of a style. ',
-                  'For example I can change just the font ',
+        p.append('This is a standard paragraph with the default style.')
+        p = Paragraph()
+        p.append('It is also possible to manully override a style. ',
+                  'This is a change of just the font ',
                   TEXT('size', size=48),
-                  ' or ',
+                  ' an this is for just the font ',
                   TEXT('typeface', font=styles.Fonts.Impact) ,
                   '.')
         section.append(p)
         return doc
-    make_char = staticmethod(make_char)
+    make_charStyleOverride = staticmethod(make_charStyleOverride)
 
-    def make_char():
+    def test_charStyleOverride(self):
+        self.doTest()
+
+    def make_charColours():
         doc, section, styles = initializeDoc()
-
-        section.append('Example 4')
         section.append('This example test changing the colour of fonts.')
-
-        #
-        #    text properties can be specified in two ways, either a
-        #    Text object can have its text properties specified like:
-        tps = TextPropertySet(colour=ss.Colours.Red)
-        text = Text('RED', tps)
+        # Text properties can be specified in two ways, either a text object
+        # can have its text properties specified via the TextPropertySet
+        # object, or by passing the colour parameter as a style.
+        red = TextPropertySet(colour=styles.Colours.Red)
+        green = TextPropertySet(colour=styles.Colours.Green)
+        blue = TextPropertySet(colour=styles.Colours.Blue)
+        yellow = TextPropertySet(colour=styles.Colours.Yellow)
         p = Paragraph()
-        p.append('This next word should be in ', text)
+        p.append('This next word should be in ')
+        p.append(Text('red', red))
+        p.append(', while the following should be in their respective ')
+        p.append('colours: ', Text('blue ', blue), Text('green ', green))
+        p.append('and ', TEXT('yellow', colour=styles.Colours.Yellow), '.')
+        # When specifying colours it is important to use the colours from the
+        # style sheet supplied with the document and not the StandardColours
+        # object each document get its own copy of the stylesheet so that
+        # changes can be made on a document by document basis without mucking
+        # up other documents that might be based on the same basic stylesheet.
         section.append(p)
         return doc
-    make_char = staticmethod(make_char)
+    make_charColours = staticmethod(make_charColours)
 
-    def make_char():
-        doc, section, styles = initializeDoc()
+    def test_charColours(self):
+        self.doTest()
 
-        #    or the shortcut TEXT function can be used like:
-        p = Paragraph()
-        p.append('This next word should be in ', TEXT('Green', colour=ss.Colours.Green))
-        section.append(p)
-
-        #    when specifying colours it is important to use the colours from the
-        #    style sheet supplied with the document and not the StandardColours object
-        #    each document get its own copy of the stylesheet so that changes can be
-        #    made on a document by document basis without mucking up other documents
-        #    that might be based on the same basic stylesheet
-
-        return doc
-    make_char = staticmethod(make_char)
