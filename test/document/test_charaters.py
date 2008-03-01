2684fb93cfeee05e0a987dd0465571c485fd96dc
Duncan McGreggor <oubiwann@gmail.com>
2008-03-01 02:28:55 +0000

    * First-run clean-up of the character tests.

--- test/document/test_charaters.py
+++ test/document/test_charaters.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python
-from rtfng import *
+from rtfng.utils import RTFTestCase
+from rtfng.Elements import Document
 
-class CharacterTestCase(TestCase):
-    def make_para():
+def initializeDoc():
+    doc = Document()
+    section = Section()
+    doc.Sections.append(section)
+    return (doc, section, doc.StyleSheet)
+
+class CharacterTestCase(RTFTestCase):
+    def make_char():
         doc, section, styles = initializeDoc()
         p = Paragraph()
-        p.append('It is also postylesible to provide overrides for element of a style. ',
+        p.append('It is also possible to provide overrides for element of a style. ',
                   'For example I can change just the font ',
                   TEXT('size', size=48),
                   ' or ',
@@ -13,48 +20,38 @@ class CharacterTestCase(TestCase):
                   '.')
         section.append(p)
         return doc
-    make_para = staticmethod(make_para)
+    make_char = staticmethod(make_char)
 
-def MakeExample4() :
-    doc     = Document()
-    ss      = doc.StyleSheet
-    section = Section()
-    doc.Sections.append( section )
+    def make_char():
+        doc, section, styles = initializeDoc()
 
-    section.append( 'Example 4' )
-    section.append( 'This example test changing the colour of fonts.' )
+        section.append('Example 4')
+        section.append('This example test changing the colour of fonts.')
 
-    #
-    #    text properties can be specified in two ways, either a
-    #    Text object can have its text properties specified like:
-    tps = TextPropertySet( colour=ss.Colours.Red )
-    text = Text( 'RED', tps )
-    p = Paragraph()
-    p.append( 'This next word should be in ', text )
-    section.append( p )
+        #
+        #    text properties can be specified in two ways, either a
+        #    Text object can have its text properties specified like:
+        tps = TextPropertySet(colour=ss.Colours.Red)
+        text = Text('RED', tps)
+        p = Paragraph()
+        p.append('This next word should be in ', text)
+        section.append(p)
+        return doc
+    make_char = staticmethod(make_char)
 
-    #    or the shortcut TEXT function can be used like:
-    p = Paragraph()
-    p.append( 'This next word should be in ', TEXT( 'Green', colour=ss.Colours.Green ) )
-    section.append( p )
+    def make_char():
+        doc, section, styles = initializeDoc()
 
-    #    when specifying colours it is important to use the colours from the
-    #    style sheet supplied with the document and not the StandardColours object
-    #    each document get its own copy of the stylesheet so that changes can be
-    #    made on a document by document basis without mucking up other documents
-    #    that might be based on the same basic stylesheet
+        #    or the shortcut TEXT function can be used like:
+        p = Paragraph()
+        p.append('This next word should be in ', TEXT('Green', colour=ss.Colours.Green))
+        section.append(p)
 
-    return doc
+        #    when specifying colours it is important to use the colours from the
+        #    style sheet supplied with the document and not the StandardColours object
+        #    each document get its own copy of the stylesheet so that changes can be
+        #    made on a document by document basis without mucking up other documents
+        #    that might be based on the same basic stylesheet
 
-
-def OpenFile( name ) :
-    return file( '%s.rtf' % name, 'w' )
-
-if __name__ == '__main__' :
-    DR = Renderer()
-
-    doc4 = MakeExample4()
-
-    DR.Write( doc4, OpenFile( '4' ) )
-
-    print "Finished"
+        return doc
+    make_char = staticmethod(make_char)
