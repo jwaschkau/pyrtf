626f80407242c29b92e07f0b581c0d5c252d97e0
Duncan McGreggor <oubiwann@gmail.com>
2008-03-13 07:47:40 +0000

    2008.03.13
    * Lots of changes to the automation script.

--- admin/syncSFRepo.py
+++ admin/syncSFRepo.py
@@ -1,16 +1,69 @@
 #!/usr/bin/env python
 import sys
+import os.path
+import pickle
+from datetime import datetime
 
 from adytum.util.sourceforge.base import login
 
+# do a date check
 filename, sfID = sys.argv[1:]
-host = 'http://sourceforge.net'
-loginURL = 'https://sourceforge.net/account/login.php'
-formURL = '%s/project/admin/svn_migration.php?group_id=%s' % (host, sfID)
-browser = login(loginURL, credFile='sourceforge_creds_test')
+now = datetime.now()
+minDays = 7
+datePickle = 'lastSync.pickle'
+picklePath = os.path.dirname(filename)
+pickleFile = os.path.join(picklePath, datePickle)
+if os.path.exists(pickleFile):
+    fh = open(pickleFile)
+    lastSync = pickle.load(fh)
+    fh.close()
+    if (now - lastSync).days < minDays:
+        print "A migration was performed less than %s day(s) ago." % minDays
+        print "Aborting ... "
+        sys.exit(1)
 
-# submit the uploaded svn dump file
+# define some URLs
+host = 'https://sourceforge.net'
+loginURL = '%s/account/login.php' % host
+formURL = '%s/project/admin/svn_migration.php?group_id=%s' % (host, sfID)
+
+# login and go to the migration page
+browser = login(loginURL, credFile='sourceforge_creds_test')
 browser.open(formURL)
 form = browser.getForm(name='migration')
+
+# check to see if the page is a report page (in which case a migration has
+# already occured, and we need to request another migration)
+if form.getControl(name='action').value == 'migration_start':
+    # this is the one we want; continue
+    pass
+elif form.getControl(name='action').value == 'migration_form':
+    form.getControl(name='button').click()
+    # we've submitted that form, now we need to get the "real" form
+    form = browser.getForm(name='migration')
+elif form.getControl(name='action').value == 'migration_progress':
+    # still waiting ...
+    print "Migration still in progress; please try later."
+    print "Aborting ... "
+    sys.exit(1)
+else:
+    # wtf?
+    print "Got unexpected form!"
+    print "\taction: %s" % form.getControl(name='action').value
+    print "Aborting ... "
+    sys.exit(1)
+
+# submit the uploaded svn dump file
 form.getControl(name='src_path_type3').value = filename
+form.getControl(name='replace_type3').value = True
 form.getControl(name='button').click()
+
+# record the time this transaction was made
+fh = open(pickleFile, 'w+')
+pickle.dump(now, fh)
+fh.close()
+
+# last words
+print "To view the results, visit the following URL:"
+print "\t%s\n" formURL
+print "\n"
