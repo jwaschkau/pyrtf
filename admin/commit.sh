8cb8d4b8bd24d7bf90a42aefa10b5c8743d50d29
Duncan McGreggor <oubiwann@gmail.com>
2008-03-13 08:34:01 +0000

    * Changed default behaviour for commit script to run tests, skipping if a flag is passed (as opposed to only running tests if a flag is passed).

--- admin/commit.sh
+++ admin/commit.sh
@@ -5,18 +5,21 @@ svn diff ChangeLog | \
 echo "Committing with this message:"
 cat commit-msg
 echo
-if [[ "$1" == 'run_tests' ]];then
-    python test/test_all.py &> test.out
-else
+FLAG='skip_tests'
+if [[ "$1" == "$FLAG" ]];then
     echo 'OK' > test.out
+else
+    # send the output (stdout and stderr) to both a file for checking and
+    # stdout for immediate viewing/feedback
+    python test/test_all.py 2>&1|tee test.out
 fi
 STATUS=`tail -1 test.out|awk '{print $1}'`
 if [[ "$STATUS" == 'OK' ]];then
     rm test.out
-    if [[ "$1" == 'run_tests' ]];then
-        echo "All tests passed."
-    else
+    if [[ "$1" == "FLAG" ]];then
         echo "Skipping tests..."
+    else
+        echo "All tests passed."
     fi
     echo "Committing to Subversion now..."
     svn commit --file commit-msg && \
@@ -24,5 +27,4 @@ if [[ "$STATUS" == 'OK' ]];then
         echo "There was an error committing; message preserved."
 else
     echo "*** Commit aborting! Test suite failed ***"
-    cat test.out
 fi
