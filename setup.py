4f87e4cca4291ebefc3999bb06cb2d8f31948415
xoviat <xoviat@users.noreply.github.com>
2017-03-28 16:30:46 -0500

    setup: change description to readme.

--- setup.py
+++ setup.py
@@ -1,9 +1,3 @@
-"""PyRTF - Rich Text Format Document Generation 
-
-PyRTF is a pure python module for the efficient generation of rich text format
-documents. Supports styles, tables, cell merging, jpg and png images and tries
-to maintain compatibility with as many RTF readers as possible. """
-
 classifiers = """\
 Development Status :: 4 - Beta
 Topic :: Text Editors :: Text Processing
@@ -11,7 +5,6 @@ Topic :: Software Development :: Libraries :: Python Modules
 Intended Audience :: Developers
 Programming Language :: Python
 License :: OSI Approved :: GNU General Public License (GPL)
-License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)
 """
 
 import sys
@@ -21,7 +14,7 @@ import versioneer
 from setuptools import setup, find_packages
 from distutils.core import setup
 
-doclines = __doc__.split("\n")
+doclines = open('README').readlines()
 
 setup(
     name='PyRTF3',
